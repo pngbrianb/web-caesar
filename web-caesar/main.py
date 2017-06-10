6f7f1be4d43cf7893d4200df9cf5bab4110a2fc2
pngbrianb <pngbrianb@gmail.com>
2017-06-09 20:00:43 -0700

    Completed. Everything seems to be in order.

--- web-caesar/main.py
+++ web-caesar/main.py
@@ -9,26 +9,26 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method="POST">
             <label for "rot"> Rotate by: </label>
             <input type="text" name="rot">
-            <textarea name="text"></textarea>
+            <textarea name="text">{0}</textarea>
             <input type="submit">
         </form>
     </body>
@@ -36,15 +36,15 @@ form = """<!DOCTYPE html>
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 
 @app.route("/", methods=['Post'])
 def encrypt():
     rotnum = int(request.form['rot'])
     msgtocode = request.form['text']
-    encrypted = rotate_string(rotnum,msgtocode)
+    encrypted = rotate_string(msgtocode,rotnum)
     display_encrypted = '<h1>'+ encrypted + '</h1>'
-    return display_encrypted
+    return form.format(encrypted)
 
 app.run()
